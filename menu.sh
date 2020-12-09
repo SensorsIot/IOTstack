6957a8aa7228d3a5e5b86e148dbdaa86ffa9e339
OneMarcFifty <marc@onemarcfifty.com>
2020-12-09 14:29:44 +0000

    added fhem menu item

--- menu.sh
+++ menu.sh
@@ -52,6 +52,7 @@ declare -A cont_array=(
 	[domoticz]="Domoticz"
 	[dozzle]="Dozzle"
 	[wireguard]="Wireguard"
+    [fhem]="Fhem"
 	# add yours here
 )
 
@@ -90,6 +91,7 @@ declare -a armhf_keys=(
 	"domoticz"
 	"dozzle"
 	"wireguard"
+    "fhem"
 	# add yours here
 )
 sys_arch=$(uname -m)
